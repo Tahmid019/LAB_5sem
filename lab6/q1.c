98e6c961f81c5c843705146376dedf0ccec02e0f
Tahmid019 <tahmidchoudhury019@gmail.com>
2025-10-28 09:42:47 +0000

    upd: mutex

--- lab6/q1.c
+++ lab6/q1.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <semaphore.h>
 #include <unistd.h>
+#include <stdatomic.h>
 
-sem_t mutex; 
+atomic_int *mutex;
+
+atomic_int init_mut(atomic_int* mutex, int a){
+    *mutex = a;
+    return *mutex;
+}
+
+atomic_int mut_wait(atomic_int *mut){
+    while(*mut <= 0);
+    --*mut;
+    return *mut;
+}
+
+atomic_int mut_signal(atomic_int *mut){
+    ++*mut;
+    return  *mut;
+}
 
 void* process(void* arg) {
     int id = *((int*)arg);
 
-    printf("Process %d waiting to enter critical section...\n", id);
-    sem_wait(&mutex); // wait (lock)
-    
-    printf("Process %d entered critical section.\n", id);
-    sleep(2); // simulate work
-    printf("Process %d leaving critical section.\n", id);
-    
-    sem_post(&mutex); // signal (unlock)
+    mut_wait(&mutex);
+
+    printf("CS Enter id: %d \n", id);
+    sleep(1);
+    printf("CS Eject id: %d \n", id);
+
+    mut_signal(&mutex);
+
     return NULL;
 }
 
@@ -23,14 +39,13 @@ int main() {
     pthread_t t1, t2;
     int id1 = 1, id2 = 2;
 
-    sem_init(&mutex, 0, 1); // init bin-sema 1
-
+    init_mut(&mutex, 1);
+    
     pthread_create(&t1, NULL, process, &id1);
     pthread_create(&t2, NULL, process, &id2);
 
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
 
-    sem_destroy(&mutex);
     return 0;
 }
